cbfafdc73029f4df2b347eb4393696bf1c730194
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-04-06 17:20:35 +0600

    t

---
+++ src/component/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h3>This the real about page</h3>
+        </div>
+    );
+};
+
+export default About;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,55 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './component/About/About';
+import Contact from './component/Contact/Contact';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+
+
+// simole router
+// const router =createBrowserRouter([
+//   {
+//     path:'/',
+//     element: <App></App>
+//   },
+//   {
+//     path:'/about',
+//     element: <About></About>
+//   },
+//   {
+//     path:'/contact',
+//     element:<Contact></Contact>
+//   }
+
+// ])
+
+
+//nasted router
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>,
+    children:[
+      {
+        path:'about',
+        element:<About></About>,
+      },
+      {
+        path:'contact',
+        element:<Contact></Contact>,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
