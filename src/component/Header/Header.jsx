57cf754d23c176c2a56ef0b04888bc61b2943a60
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-04-06 17:32:10 +0600

    f

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <a href='/'>Home</a>
-            <a href='/about'>About</a>
-            <a href='/contact'>Contact</a>
-        </div>
+        <nav>
+            {/* 'a' tag dile load hoy tei react e amra 'Link' use korbo r 'href' er jay gay 'to' likhbo */}
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+        </nav>
     );
 };
 
